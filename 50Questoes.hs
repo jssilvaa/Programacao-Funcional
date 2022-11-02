6538e59df0be88780f8cbccc7ce22a10d8a6658e
jssilvaa <117296619+jssilvaa@users.noreply.github.com>
2022-11-02 19:02:46 +0000

    Update 50Questoes.hs
    Remoção das descrições de modo a aumentar a legibilidade e limpeza do código.

--- 50Questoes.hs
+++ 50Questoes.hs
@@ -1,186 +1,186 @@
 {- 50 Questões 2022/2023 -}
 
--- 1 -- Constrói uma lista de inteiros compreendidos entre x e y.
+-- 1 -- 
 enumFromTo' :: Int -> Int -> [Int]
 enumFromTo' x y 
    | x > y = []
    | otherwise = x : enumFromTo' (x+1) y
 
--- 2 -- Constrói uma lista de inteiros entre p e r em progressão aritmética de q-p.
+-- 2 -- 
 enumFromThenTo' :: Int -> Int -> Int -> [Int]
 enumFromThenTo' p q r 
    | p > r = []
    | otherwise = p : enumFromThenTo' (p + (q-p)) (q + (q-p)) r
 
--- 3 -- Definição do operador (++) da Prelude.
+-- 3 -- 
 concatena :: [a] -> [a] -> [a]
 concatena x [] = x
 concatena [] x = x
 concatena (x:xs) y = x : concatena xs y
 
--- 4 -- Definição do operador (!!) da Prelude. 
+-- 4 -- 
 posicao :: [a] -> Int -> Maybe a
 posicao l x | null l || x < 0 = Nothing
 posicao (x:xs) 0 = Just x
 posicao (x:xs) n = posicao xs (n-1)
 
--- 5 -- Reverte uma lista (reverse na Prelude).
+-- 5 -- 
 reverse' :: [a] -> [a]
 reverse' [] = []
 reverse' x = last x : reverse' (init x )
 
--- 6 -- take list op. from the Prelude library.
+-- 6 -- 
 take' :: Int -> [a] -> [a]
 take' _ [] = []
 take' x (y:ys)
    | x <= 0 = []
    | otherwise = y : take (x-1) ys
 
--- 7 -- drop list op. from the Prelude library.
+-- 7 -- 
 drop' :: Int -> [a] -> [a]
 drop' _ [] = []
 drop' x (y:ys) 
    | x <= 0 = y 
    | otherwise = drop' (x-1) ys
 
--- 8 -- zip function from the Prelude library.
+-- 8 -- 
 zip' :: [a] -> [b] -> [(,) a b]
 zip' (x:xs) (y:ys) = (x,y) : zip' xs ys
 zip' _ _ = []
 
--- 9 -- replicate function from the Prelude library (sem guardas).
+-- 9 -- 
 replicate' :: Int -> a -> [a]
 replicate' 0 _ = []
 replicate' n x = x : replicate' (n-1) x
 
--- 10 -- intersperse an element linking each element of the given list.
+-- 10 
 intersperse' :: a -> [a] -> [a]
 intersperse' _ [] = []
 intersperse' _ [x] = [x] 
 intersperse' x (h:t) = h : x : intersperse' x t  
 
--- 11 -- group function from the Prelude library. 
+-- 11 -- 
 group' :: Eq a => [a] -> [[a]]
 group' [] = []
 group' x = p : group' (length p `drop` x) 
    where p = takeWhile (== head x) x 
 
--- 12 -- concat function from the Prelude library. 
+-- 12 -- 
 concat' :: [[a]] -> [a]
 concat' = foldr (++) []
 
--- 13 -- inits function (determines a list's set of prefixes).
+-- 13 -- 
 inits' :: [a] -> [[a]]
 inits' [] = [[]]
 inits' l = inits' (init l) ++  [l]
 
--- 14 -- tails function from the prelude library.
+-- 14 --
 tails' :: [a] -> [[a]]
 tails' [] = [[]]
 tails' l@(h:t) = l : tails' t
 
--- 15 -- receives a set of sets of a and outputs the set containing the first element of each set of a. 
+-- 15 --  
 heads'' :: [[a]] -> [a]
 heads'' = map head
 
--- 16 -- Receives a list of lists and outputs the total element count.
+-- 16 --
 total' :: [[a]] -> Int
 total' = sum . map length
 
 total'' :: [[a]] -> Int
 total'' = foldr ((+) . length) 0
 
--- 17 -- Receives a list of triples and yields a list of tuples excluding the second component.
+-- 17 -- 
 fun :: [(a,b,c)] -> [(a,c)]
 fun = map (\(x,y,z) -> (x,z))
 
--- 18 -- Recebe uma lista de triplos com uma string na primeira posição, e dá output ao todo das strings concatenadas.
+-- 18 -- 
 cola :: [(String, b, c)] -> String
 cola = concatMap (\(x,y,z) -> x)
 
--- 19 -- Recebe um ano, uma idade, uma lista de (nomes,ano de nascimento) e dá output aos nomes das pessoas que já completaram ou vão completar "idade" no "ano" fornecido. 
+-- 19 --
 idade' :: Int -> Int -> [(String, Int)] -> [String]
 idade' year age = map fst . filter (\(s,y) -> year - y >= age)
 
--- 20 -- Constrói uma lista de potências n^0, .., n^(m-1).
+-- 20 -- 
 powerEnumFrom' :: Int -> Int -> [Int]
 powerEnumFrom' n m = [ n^x | x <- [0..m-1] ]
 
--- 21 -- Verifica se um número é primo.
+-- 21 --
 isPrime :: Int -> Bool
 isPrime n = null [m | m <- [2..n'], mod n m == 0]
    where n' = ceiling(sqrt(fromIntegral n))
 
--- 22 -- Verifica se a lista dada é um prefixo da outra.
+-- 22 --
 isPrefixOf' :: (Eq a) => [a] -> [a] -> Bool
 isPrefixOf' [] _ = True
 isPrefixOf' _ [] = False
 isPrefixOf' (x:xs) (y:ys) = x == y && isPrefixOf' xs ys
 
--- 23 -- Verifica se a lista dada é um sufixo da outra.
+-- 23 -- 
 isSuffixOf' :: (Eq a) => [a] -> [a] -> Bool
 isSuffixOf' [] _ = True
 isSuffixOf' _ [] = False
 isSuffixOf' x y = (==) x (drop r y)
    where r = length y - length x
 
--- 24 -- Testa se a lista dada é uma subsequência da outra (subset na mesma ordem relativa).
+-- 24 -- 
 isSubsequenceOf' :: (Eq a) => [a] -> [a] -> Bool
 isSubsequenceOf' [] _ = True
 isSubsequenceOf' _ [] = False
 isSubsequenceOf' (x:xs) y = elem x y && isSubsequenceOf'' xs (dropWhile (/= x) y)
 
--- 25 -- Enumera os índices das ocorrências de um elemento na lista.
+-- 25 -- 
 elemIndices' :: Eq a => a -> [a] -> [Int]
 elemIndices' _ [] = []
 elemIndices' x (h:t)
    | x == h = 0 : map (+1) (elemIndices' x t)
    | otherwise = map (+1) (elemIndices' x t)
 
--- 26 -- Calcula a mesma lista sem repetições.
+-- 26 -- 
 nub' :: Eq a => [a] -> [a]
 nub' [] = []
 nub' (h:t)
    | elem h t = nub' t
    | otherwise = h : nub' t
 
--- 27 -- Retorna a lista depois de removida a primeira ocorrência do elemento fornecido.
+-- 27 --
 delete :: Eq a => a -> [a] -> [a]
 delete _ [] = []
 delete x (h:t)
    | x == h = t
    | otherwise = h : delete x t
 
--- 28 -- Retorna a lista depois de removidas as primeiras ocorrências de uma série de elementos de outra lista.
+-- 28 -- 
 excepto :: Eq a => [a] -> [a] -> [a]
 excepto l [] = l
 excepto x [y] = delete y x
 excepto x (y:ys) = delete y (excepto x ys)
 
--- 29 -- Calcula a união de duas listas, sem repetir os elementos da segunda que ocorrem na primeira.
+-- 29 -- 
 union :: Eq a => [a] -> [a] -> [a]
 union x y = (++) x (filter (`notElem` x) y)
 
--- 30 -- Remove os elementos da primeira lista que não pertencem à segunda.
+-- 30 --
 intersect :: Eq a => [a] -> [a] -> [a]
 intersect x y = filter (`elem` y) x
 
--- 31 -- Insere ordenadamente um elemento na lista. 
+-- 31 -- 
 insert :: Ord a => a -> [a] -> [a]
 insert x [] = [x]
 insert x l@(h:t)
    | h < x = h : insert x t
    | h >= x =  x : l
    
--- 32 -- Junta as strings de uma lista com um espaço.
+-- 32 --
 unwords' :: [String] -> String
 unwords' = init . concatMap (flip (++) " ")
 
--- 33 -- Junta as strings de uma lista com um "\newline".
+-- 33 --
 unlines'' :: [String] -> String
 unlines'' = concatMap (flip (++) "\n")
 
--- 34 -- Retorna o índice do maior elemento da lista.
+-- 34 -- 
 pMaior :: Ord a => [a] -> Int 
 pMaior [_] = 0
 pMaior (h:t)
@@ -188,56 +188,55 @@ pMaior (h:t)
    | otherwise = 0
    where i = pMaior t
 
--- 35 -- Filtra o primeiro b cujo a satisfaz a condição dada (o enunciado da questão não bate certo com a definição).
+-- 35 -- 
 lookup' :: Eq a => a -> [(a,b)] -> Maybe b
 lookup' a x = if null p then Nothing else Just $ (snd . head) p
    where p = filter (\(x',_) -> x' == a) x
 
--- 36 -- Calcula o maior prefixo crescente de uma lista. 
+-- 36 --
 prefixo :: Ord a => [a] -> [a]
 prefixo (x:y:xs) = if x <= y then x : prefixo (y:xs) else [x]
 prefixo [x] = [x]
 prefixo _ = []
 
--- 37 -- Insertion Sort.
+-- 37 -- 
 iSort :: Ord a => [a] -> [a]
 iSort [] = []
 iSort (x:xs) = insert x (iSort xs)
 
--- 38 -- True se a primeira string for menor lexicograficamente que a primeira, false caso contrário.
+-- 38 -- 
 menor :: String -> String -> Bool
 menor [] _ = True
 menor _ [] = False
 menor (x:xs) (y:ys) = if x == y then menor xs ys else x < y
 
--- 39 -- Testa se o elemento pertence ao multiset.
+-- 39 -- 
 type MSet a = [(a,Int)]
-
 elemMSet :: Eq a => a -> MSet a -> Bool
 elemMSet z = any (\(x,y) -> (==) x z) 
 
--- 40 -- Converte um multiset na lista dos seus elementos.
+-- 40 -- 
 converteMSet :: MSet a -> [a]
 converteMSet = concatMap (\(x,y) -> replicate y x) 
 
--- 41 -- Insere um elemento no multiset. 
+-- 41 -- 
 insereMSet :: Eq a => a -> MSet a -> MSet a 
 insereMSet z ms 
    | any (\(x,y) -> (== z) x) ms = [if z == x then (x,y+1) else (x,y) | (x,y) <- ms]
    | otherwise = (z,1) : ms
 
--- 42 -- Remove um elemento ao multiconjunto.
+-- 42 -- 
 removeMSet :: Eq a => a -> MSet a -> MSet a 
 removeMSet z ms = filter (\(x,y) -> y /= 0) [if z == x then (x, y-1) else (x,y) | (x,y) <- ms]
 
--- 43 -- Constrói um multiset a partir de uma lista ORDENADA de a's.
+-- 43 -- 
 constroiMSet :: Ord a => [a] -> MSet a 
 constroiMSet [] = []
 constroiMSet x = (head p, r) : constroiMSet (drop r x)
     where p = [y | y <- x, y == head x]
           r = length p 
 
--- 44 -- Divide uma lista de Eithers em duas listas.
+-- 44 -- 
 partitionEithers' :: [Either a b] -> ([a],[b])
 partitionEithers' [] = ([],[])
 partitionEithers' ((Left l):t) = (l : ls, rs)
@@ -245,21 +244,20 @@ partitionEithers' ((Left l):t) = (l : ls, rs)
 partitionEithers' ((Right r):t) = (ls, r : rs)
    where (ls, rs) = partitionEithers' t
    
--- 45 -- Coleciona os elementos a de uma lista de maybes.
+-- 45 --
 catMaybes' :: [Maybe a] -> [a]
 catMaybes' [] = []
 catMaybes' (Just x : xs) = x : catMaybes' xs  
 catMaybes' (_:xs) = catMaybes' xs 
 
 -- 46 -- 
-data Movimento = Norte | Sul | Este | Oeste
-               deriving (Show, Eq)
-               
+data Movimento = Norte | Sul | Este | Oeste deriving (Show, Eq)
+
 mov :: (Int,Int) -> [Movimento] 
 mov (x,y) = p ++ q
    where p = if x >= 0 then replicate x Este else replicate (-x) Oeste
          q = if y >= 0 then replicate y Norte else replicate (-y) Sul
-
+         
 caminho' :: (Int,Int) -> (Int,Int) -> [Movimento]
 caminho' (a,b) (c,d) = mov (c-a, d-b)
 
