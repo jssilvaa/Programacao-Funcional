232996ac1ef7360aa39c6e708591b8ab5b9dc796
jssilvaa <117296619+jssilvaa@users.noreply.github.com>
2023-01-08 11:31:20 +0000

    Update AulaT13.hs

--- Aulas/AulaT13.hs
+++ Aulas/AulaT13.hs
@@ -16,13 +16,14 @@ instance Show Exp where
    show (Const x) = show x 
    show (Op o e1 e2) = "(" ++ show e1 ++ [' ', o, ' '] ++ show e2 ++ ")"
 
+-- Calcula o valor de uma Exp --
 calcula :: Exp -> Int 
 calcula (Const x) = x 
 calcula (Op o e1 e2) = case o of '+' -> calcula e1 + calcula e2 
                                  '-' -> calcula e1 - calcula e2 
                                  '*' -> calcula e1 * calcula e2 
-                                 _ -> 0 
-
+                                   _ -> 0 
+-- Parte uma string em [(operador, e1, e2)] -- 
 parte :: String -> [(Char, String, String)]
 parte s = if (b == "") then []
           else (x, a, xs) : [ (p, a ++ (x:q), r) | (p,q,r) <- l]
@@ -31,14 +32,17 @@ parte s = if (b == "") then []
                 (x:xs) = b   
                 l = parte xs 
 
+-- Gera todas as expressões que se podem obter a partir de uma string (input) -- 
 geraAlt :: String -> [Exp]
 geraAlt [] = []
 geraAlt s = if null a then [Const (read s)]
             else [Op x s1 s2 | (x, e1, e2) <- a, s1 <- geraAlt e1, s2 <- geraAlt e2]
     where a = parte s          
 
+-- Dada uma string (expressão) e um valor Int a procurar, 
+-- retorna (maybe) a string (expressão) com os parênteses que permite obter
+-- esse valor.
 solve :: String -> Int -> Maybe String 
 solve [] _ = Nothing 
 solve s x = if null p then Nothing else Just $ ((fst . head) p)
     where p = filter (\(_,y) -> x == y) [(show e, calcula e) | e <- geraAlt s]
-
